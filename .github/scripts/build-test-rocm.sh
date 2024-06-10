e10a89d894e0f5203051b708164de15df2b62864
pnunna93 <104791500+pnunna93@users.noreply.github.com>
2024-06-10 17:09:56 -0500

    Update build-test-rocm.sh

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -7,8 +7,8 @@ if [ "${build_os:0:6}" == ubuntu ]; then
 	image=rocm/pytorch:latest
 	echo "Using image $image"
 	docker run --platform "linux/$build_arch" --rm -i -w /src -v "$PWD:/src" "$image" sh -c \
-		"sudo apt-get update \
-      && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends cmake \
+		"apt-get update \
+      && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
       && cmake -DCOMPUTE_BACKEND=hip . \
       && cmake --build . \
       && pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
