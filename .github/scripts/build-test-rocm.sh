2af3202d58fa131119a840ba27f93771becc7d41
pnunna93 <104791500+pnunna93@users.noreply.github.com>
2024-06-14 14:23:55 -0500

    Create separate functions for testing

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -3,20 +3,39 @@ declare build_arch
 declare build_os
 
 set -xeuo pipefail
+
+# Function to cleanup Docker container
+cleanup() {
+    echo "Cleaning up the container..."
+    docker stop --time=0 bnb_rocm_test 
+    docker rm bnb_rocm_test 
+}
+ 
+# Function to build and test bitsandbytes
+build_and_test() {
+    echo "Building bitsandbytes..."
+    docker exec bnb_rocm_test sh -c \
+        "apt-get update \
+&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
+&& pip install -r requirements-ci.txt \
+&& cmake -DCOMPUTE_BACKEND=hip . \
+&& cmake --build . \
+&& echo \"Running Unittests...\" \
+&& pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
+}
+
+# Function to setup Container
+setup_container() {
+    image=rocm/pytorch:latest
+    echo "Using image $image"
+    docker run --platform "linux/$build_arch" -i -d -w /src --device=/dev/kfd --device=/dev/dri --group-add video --name bnb_rocm_test "$image"
+    docker cp $PWD bnb_rocm_test:/src
+}
+
 if [ "${build_os:0:6}" == ubuntu ]; then
-	image=rocm/pytorch:latest
-	echo "Using image $image"
-	docker run --platform "linux/$build_arch" -i -d -w /src --device=/dev/kfd --device=/dev/dri --group-add video --name bnb_rocm_test "$image"
-        docker cp $PWD bnb_rocm_test:/src
-        docker exec bnb_rocm_test sh -c \
-		"apt-get update \
-      && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
-      && pip install -r requirements-ci.txt \
-      && cmake -DCOMPUTE_BACKEND=hip . \
-      && cmake --build . \
-      && pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
-        docker stop --time=0 bnb_rocm_test
-	docker rm bnb_rocm_test
+	setup_container
+        build_and_test
+	cleanup
 fi
 
 output_dir="output/${build_os}/${build_arch}"
