511aabb81eb43fb3c9fd7748e0111c487272630e
pnunna93 <104791500+pnunna93@users.noreply.github.com>
2024-06-14 13:50:12 -0500

    Update build-test-rocm.sh

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -6,14 +6,17 @@ set -xeuo pipefail
 if [ "${build_os:0:6}" == ubuntu ]; then
 	image=rocm/pytorch:latest
 	echo "Using image $image"
-	docker run --platform "linux/$build_arch" --rm -i -w /src -v "$PWD:/src" \
-        --device=/dev/kfd --device=/dev/dri --group-add video "$image" sh -c \
+	docker run --platform "linux/$build_arch" -i -d -w /src --device=/dev/kfd --device=/dev/dri --group-add video --name bnb_rocm_test "$image"
+        docker cp $PWD bnb_rocm_test:/src
+        docker exec bnb_rocm_test sh -c \
 		"apt-get update \
       && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
       && pip install -r requirements-ci.txt \
       && cmake -DCOMPUTE_BACKEND=hip . \
       && cmake --build . \
       && pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
+        docker stop --time=0 bnb_rocm_test
+	docker rm bnb_rocm_test
 fi
 
 output_dir="output/${build_os}/${build_arch}"
