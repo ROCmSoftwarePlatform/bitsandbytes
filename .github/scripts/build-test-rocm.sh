01ce1a315b798ff0593459678af62055ef56f499
Prasanth Nunna <Prasanth.Nunna@amd.com>
2024-06-18 22:31:34 +0000

    Remove additional packages

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -6,20 +6,12 @@ set -xeuo pipefail
 if [ "${build_os:0:6}" == ubuntu ]; then
 	image=rocm/dev-ubuntu-22.04:6.1-complete
 	echo "Using image $image"
-	docker run --rm --platform "linux/$build_arch" -i -w /src -v "$PWD:/src" --device=/dev/kfd \
-		--device=/dev/dri --group-add video "$image" sh -c \
+	docker run --rm --platform "linux/$build_arch" -i \
+		-w /src -v "$PWD:/src" "$image" sh -c \
 		"apt-get update \
-      && apt-get install -y software-properties-common \
-      && add-apt-repository ppa:deadsnakes/ppa \
-      && apt-get update \
-      && apt-get install -y python3.9-dev python3-pip \
-      && pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.1/ \
       && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
-      && pip install -r requirements-ci.txt \
       && cmake -DCOMPUTE_BACKEND=hip . \
-      && cmake --build . \
-      && echo \"Running Unittests...\" \
-      && pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
+      && cmake --build ."
 fi
 
 #output_dir="output/${build_os}/${build_arch}"
