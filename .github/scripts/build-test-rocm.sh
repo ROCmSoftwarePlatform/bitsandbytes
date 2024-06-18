43bc3175f43654e9819fd6be0d34ed702360b350
Prasanth Nunna <Prasanth.Nunna@amd.com>
2024-06-18 01:44:31 +0000

    Update ci scripts

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -3,43 +3,18 @@ declare build_arch
 declare build_os
 
 set -xeuo pipefail
-
-# Function to cleanup Docker container
-cleanup() {
-    echo "Cleaning up the container..."
-    docker stop --time=0 bnb_rocm_test
-    docker rm bnb_rocm_test
-}
-
-# Function to build and test bitsandbytes
-build_and_test() {
-    trap 'cleanup' ERR
-    echo "Building bitsandbytes..."
-    docker exec bnb_rocm_test sh -c \
-        "apt-get update \
-&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
-&& cd bitsandbytes \
-&& pip install -r requirements-ci.txt \
-&& cmake -DCOMPUTE_BACKEND=hip . \
-&& cmake --build . \
-&& echo \"Running Unittests...\" \
-&& pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
-}
-
-# Function to setup Container
-setup_container() {
-    image=rocm/pytorch:latest
-    echo "Using image $image"
-    cleanup
-    docker run --platform "linux/$build_arch" -i -d -w /src --device=/dev/kfd --device=/dev/dri --group-add video --name bnb_rocm_test "$image"
-    trap 'cleanup' ERR
-    docker cp $PWD bnb_rocm_test:/src
-}
-
 if [ "${build_os:0:6}" == ubuntu ]; then
-	setup_container
-        build_and_test
-	cleanup
+	image=rocm/pytorch:latest
+	echo "Using image $image"
+	docker run --rm --platform "linux/$build_arch" -i -w /src -v "$PWD:/src" --device=/dev/kfd \
+		--device=/dev/dri --group-add video -"$image" sh -c \
+		"apt-get update \
+      && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
+      && pip install -r requirements-ci.txt \
+      && cmake -DCOMPUTE_BACKEND=hip . \
+      && cmake --build . \
+      && echo \"Running Unittests...\" \
+      && pytest --log-cli-level=DEBUG --continue-on-collection-errors tests"
 fi
 
 #output_dir="output/${build_os}/${build_arch}"
