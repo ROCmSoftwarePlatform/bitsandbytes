a4177e83545451c66d02446188dea44825a9452a
Prasanth Nunna <Prasanth.Nunna@amd.com>
2024-06-18 22:09:18 +0000

    Switch to ubuntu base docker

--- .github/scripts/build-test-rocm.sh
+++ .github/scripts/build-test-rocm.sh
@@ -4,11 +4,16 @@ declare build_os
 
 set -xeuo pipefail
 if [ "${build_os:0:6}" == ubuntu ]; then
-	image=rocm/pytorch:latest
+	image=rocm/dev-ubuntu-22.04:6.1-complete
 	echo "Using image $image"
 	docker run --rm --platform "linux/$build_arch" -i -w /src -v "$PWD:/src" --device=/dev/kfd \
 		--device=/dev/dri --group-add video "$image" sh -c \
 		"apt-get update \
+      && apt-get install -y software-properties-common \
+      && add-apt-repository ppa:deadsnakes/ppa \
+      && apt-get update \
+      && apt-get install -y python3.9-dev python3-pip \
+      && pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.1/ \
       && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake \
       && pip install -r requirements-ci.txt \
       && cmake -DCOMPUTE_BACKEND=hip . \
